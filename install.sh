ef87494d32f177b820c498b1b7772369ef46198b
Markus Frings <email@markusfrings.eu>
2021-02-02 09:20:01 +0100

    Add more install.

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env zsh
 
-dotfiles_path=$0:A
+dotfiles_path=${0:a:h}
 
-echo "Dotfiles are installed in $dotfiles_path"
+echo "Install ZSH and oh-my-zsh ..."
 
+if [ -d "$HOME/.oh-my-zsh" ]; then
+  echo "  Backup existing .oh-my-zsh installation"
+  mv $HOME/.oh-my-zsh $HOME/.oh-my-zsh.bak
+fi
+
+if [ -L "$HOME/.oh-my-zsh" ]; then
+  echo "  Remove existing link"
+  rm -f $HOME/.oh-my-zsh
+fi
+
+ln -s $dotfiles_path/.oh-my-zsh $HOME/.oh-my-zsh
+
+if [ -e "$HOME/.zshrc
